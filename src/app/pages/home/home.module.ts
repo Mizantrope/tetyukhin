7d342f59185ca4fad14554147d15420ced3767a8
Michail Chirsky <mch@virtoway.com>
2019-12-10 16:48:31 +0200

    Remove ngx-masonry, added shuffle js

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { MasonryGalleryModule } from 'ngx-masonry-gallery';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, HomeRoutingModule, SharedModule, MasonryGalleryModule]
+    imports: [CommonModule, HomeRoutingModule, SharedModule]
 })
 export class HomeModule {}
