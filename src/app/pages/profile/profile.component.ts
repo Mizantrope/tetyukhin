3c7de60370687373129c0f5eb6e0e46fe59a5bed
Michail Chirsky <mch@virtoway.com>
2019-12-11 16:53:05 +0200

    Update modules

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-profile',
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
+export class ProfileComponent {
+    public items = new Array(5);
+    public view = 'achievements';
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ImagePipe } from './pipes/image.pipe';
 import { LoaderComponent } from './components/loader/loader.component';
+import { TilesComponent } from './components/tiles/tiles.component';
 
 @NgModule({
-    declarations: [ImagePipe, LoaderComponent],
+    declarations: [ImagePipe, LoaderComponent, TilesComponent],
     imports: [CommonModule],
-    exports: [ImagePipe, LoaderComponent],
+    exports: [ImagePipe, LoaderComponent, TilesComponent],
     providers: [ImagePipe]
 })
 export class SharedModule {}
